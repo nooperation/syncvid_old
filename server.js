bf28404e88d01900f4b42f49a7c8b85844f5205f
nooperation <madbrahmin@gmail.com>
2016-11-23 19:01:09 -0500

    Renamed index -> room

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var shortid = require('shortid');
 
 var Room = function (room_name, owner) {
   this.room_name = room_name;
@@ -87,7 +88,13 @@ var Room = function (room_name, owner) {
 var rooms = {};
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.redirect('/room/' + shortid.generate());
+});
+app.get('/room', function (req, res) {
+  res.redirect('/room/' + shortid.generate());
+});
+app.get('/room/:room_name', function (req, res) {
+  res.sendFile(__dirname + '/room.html');
 });
 
 io.on('connection', function (socket) {
